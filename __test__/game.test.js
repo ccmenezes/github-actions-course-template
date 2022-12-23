fc4aee77962c4241035fb977bde8737acc1afdd3
Claudia Menezes <claudia@claudia-asus.Home>
2022-12-23 00:02:18 +0000

    removed blank line

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -4,7 +4,6 @@ const fs = require('fs')
 describe('App', () => {
   it('Contains the compiled JavaScript', async () => {
     fs.readFile('./public/main.js', 'utf8', (err, data) => {
-
       expect(err).toBe(null)
       expect(data).toMatchSnapshot()
     })
